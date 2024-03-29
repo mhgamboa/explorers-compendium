16249a076fb6ada8ac38e56103611ec154af5139
NomadikLiving <marcus@nomadikliving.com>
2024-03-29 16:16:54 -0600

    Update initiativeModal store subscriber

--- src/components/combat/modal/Initiative.tsx
+++ src/components/combat/modal/Initiative.tsx
@@ -1,9 +1,10 @@
+"use client";
 import React from "react";
 import ModalWrapper from "@/components/ui/ModalWrapper";
 import { useCombatStore } from "@/store/combatStore";
 
 export default function Initiative() {
-  const { view } = useCombatStore();
+  const view = useCombatStore((state) => state.view);
 
   if (view !== "initiative") return;
   return <ModalWrapper>Initiative</ModalWrapper>;
